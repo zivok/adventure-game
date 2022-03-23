63045e5048cc12dafb8a44cd14a57b713347c0d4
Jhonatan Yupanqui <jnyupanqui.zw@hotmail.com>
2022-03-23 07:45:38 -0700

    start description

---
+++ game.py
@@ -0,0 +1,10 @@
+import time;
+
+print ("You find yourself standing in an open field, "
++ "filled with grass and yellow wildflowers.")
+time.sleep(2)
+
+print ("Rumor has it that a wicked fairie is somewhere " 
++ "around here, and has been terrifying the nearby village.")
+time.sleep(2)
+
