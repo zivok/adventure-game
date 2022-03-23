184bcf7e9d7e254ecc7eb9a7cb188d2f397b5f5d
Jhonatan Yupanqui <jnyupanqui.zw@hotmail.com>
2022-03-23 07:57:16 -0700

    choice

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import time;
 
+""" description """
 print ("You find yourself standing in an open field, "
 + "filled with grass and yellow wildflowers.")
 time.sleep(2)
@@ -7,4 +8,23 @@ time.sleep(2)
 print ("Rumor has it that a wicked fairie is somewhere " 
 + "around here, and has been terrifying the nearby village.")
 time.sleep(2)
+
+""" choice """
+print ("Enter 1 to knock on the door of the house.")
+time.sleep(2)
+
+print ("Enter 2 to peer into the cave.")
+time.sleep(2)
+
+print ("What would you like to do?")
+time.sleep(2)
+
+choice = input("(Please enter 1 or 2).")
+
+if choice == "1":
+    print ("option 1")
+elif choice == "2":
+    print ("option 2")
+else:
+    print ("invalid option")
 
